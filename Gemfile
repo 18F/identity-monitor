c62d8978803ae256528cda5b0005b471a43fe9ca
Robb Shecter <robb.shecter@gsa.gov>
2016-11-08 21:45:55 +0000

    Added Dotenv

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ ruby '2.3.1'
 
 gem 'capybara'
 gem 'capybara-webkit'
+gem 'dotenv'
 gem 'rspec-webservice_matchers'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'capybara'
 require 'capybara/rspec'
 require 'capybara/webkit'
-
+require 'dotenv'
 require 'rspec/webservice_matchers'
+
+Dotenv.load
 
 Capybara.javascript_driver = :webkit
 Capybara.run_server        = false
