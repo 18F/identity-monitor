e9369abceae09f6a0f9fd21f77cf34554ffdbb2d
Robb Shecter <robb.shecter@gsa.gov>
2016-11-23 01:10:12 -0800

    Basic webhook handlers

--- webhooks_demo.rb
+++ webhooks_demo.rb
@@ -2,7 +2,18 @@
 require 'twilio-ruby'
 require 'sinatra'
 
+
 post '/sms' do
   body = params['Body']
-  puts body
+  puts "Got an SMS with contents: #{body}"
+end
+
+
+post '/MessageStatus' do
+  message_sid    = params.fetch('MessageSid')
+  message_status = params.fetch('MessageStatus')
+
+  puts "SID: #{message_sid}, Status: #{message_status}"
+
+  response.status = 204
 end
